54249b6436e953a9a90a948f7416d742f59326ec
Syed Shalish Raza <59282850+syedraza98@users.noreply.github.com>
2022-06-03 08:07:06 +0530

    Add files via upload

---
+++ classesandobjects1/Studentuse1.java
@@ -0,0 +1,17 @@
+package classesandobjects1;
+
+public class Studentuse1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1=new Student();
+		s1.name="Sunny";
+		s1.roll=1;
+		System.out.print(s1.name);
+		System.out.print(s1.roll);
+		//System.out.print(s1.id);
+		System.out.print(s1.add(3,5));
+
+	}
+
+}
