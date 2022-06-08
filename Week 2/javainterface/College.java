cd5f789246458ed85fc28004f322c05bb007486a
Syed Shalish Raza <59282850+syedraza98@users.noreply.github.com>
2022-06-08 12:50:26 +0530

    Delete Week 2/javainterface directory

--- Week 2/javainterface/College.java
+++
@@ -1,9 +0,0 @@
-package javainterface;
-
-public interface College {
-		int numofstudents=100;
-		int numberOfCourse(int course);
-		int yearOfAdmission(int year);
-		
-		
-}
--- Week 2/javainterface/university.java
+++
@@ -1,7 +0,0 @@
-package javainterface;
-
-public interface university {
-	boolean isPartof() ;
-		
-	
-}
