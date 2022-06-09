ecde931295cbbf891cd833b6414598e2aaa0d39f
Syed Shalish Raza <59282850+syedraza98@users.noreply.github.com>
2022-06-09 16:31:55 +0530

    Add files via upload

---
+++ Week1/WrapperClass/WrapperClass.java
@@ -0,0 +1,16 @@
+package WrapperClass;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=8;
+		Integer ii= new Integer(9);
+		System.out.println(ii.intValue());
+		ii=i;
+		System.out.println(ii.intValue());
+		
+
+	}
+
+}
