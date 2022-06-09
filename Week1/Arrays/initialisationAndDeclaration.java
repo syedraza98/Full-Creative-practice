efd84a39f5ce6331043e1a55a30991ec4347a35e
Syed Shalish Raza <59282850+syedraza98@users.noreply.github.com>
2022-06-09 16:32:53 +0530

    Add files via upload

---
+++ Week1/Arrays/initialisationAndDeclaration.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class initialisationAndDeclaration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		int [] arr = new int[6]; 
+		arr[1]=10;
+		arr[4]=20;
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		int arr1 [] = {1,2,3,4};
+		System.out.println(arr1[3]);
+
+	}
+
+}
