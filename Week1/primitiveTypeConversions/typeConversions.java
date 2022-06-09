807f6acd7c27f49bf6dc94310734898689af65ae
Syed Shalish Raza <59282850+syedraza98@users.noreply.github.com>
2022-06-09 10:44:27 +0530

    Add files via upload

---
+++ Week1/primitiveTypeConversions/typeConversions.java
@@ -0,0 +1,18 @@
+package primitiveTypeConversions;
+
+public class typeConversions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int integer=25;
+		double val=integer;
+		System.out.println(val);
+		long l=35;
+		System.out.println(l);
+		integer=(int)l;
+		System.out.println(integer);
+		
+
+	}
+
+}
